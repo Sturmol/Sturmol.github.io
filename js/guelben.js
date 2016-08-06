a38f093efa6adce2d6b83454fc89992d21f9929c
Sturmol <sturmol@hotmail.com>
2016-08-06 23:52:43 +0200

    Showcase updated

--- js/guelben.js
+++ js/guelben.js
@@ -3,22 +3,56 @@
 	'use strict';
 
 	$(document).ready(function () {
-		$('nav, nav a').hover(function () {
-		$(this).find('.slide-in').toggleClass('hidden');
-	}, function () {
-		$(this).find('.slide-in').toggleClass('hidden');
-	});
 
-	$('nav a').click(function (e) {
-		e.preventDefault();
-		var target = this.hash,
-			$target = $(target);
-	$('html, body').stop().animate({
-		'scrollTop': $target.offset().top
-	}, 1200, 'easeInOutCubic', function () {
-		window.location.hash = target;
+			$('nav').hover(function () {
+			$(this).find('.slide-in').toggleClass('weak');
+		}, function () {
+			$(this).find('.slide-in').toggleClass('weak');
+		});
+
+		$('nav a').hover(function () {
+				$(this).find('.slide-in').removeClass('weak');
+			}, function () {
+				$(this).find('.slide-in').addClass('weak');
+			});
+
+		$('.section-nav').click(function (e) {
+			e.preventDefault();
+			var target = this.hash,
+				$target = $(target);
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 1200, 'easeInOutCubic', function () {
+				window.location.hash = target;
+			});
+		});
+
+		$('.menu-clickback').click(function (e) {
+			e.preventDefault();
+			var $pulsates = $('.pulsate-in');
+			$pulsates.hide('fast');
+			$('html, body').scrollTop(0, function () {
+				window.location.hash = '#';
+			});
+			$pulsates.each(function () {
+			$(this).show('pulsate');
+			});
+		});
+
+		$('.slider').slider({full_width: true});
+
+		$('#nav, footer').on('scrollSpy:enter', function () {
+			$('html, body').stop();
+			$('.main-title-header').show('pulsate');
+		});
+
+		$('#nav, footer').on('scrollSpy:exit', function () {
+			$('.main-title-header').hide('pulsate');
+		});
+
+		$('#nav, footer').scrollSpy();
 	});
-  });
-});
 
 }());
+
+
